81ec6d9e198ba0cf22dfa91158cbfb3f74fcf2ac
roshan <roshan@roshans-MacBook-Pro.local>
2024-03-29 13:16:01 +1100

    added routes and layout

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,22 +1,46 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Navigate, createBrowserRouter } from 'react-router-dom';
 import Signup from './views/Signup';
 import Users from './views/Users';
 import NotFound from './views/NotFound';
 import Login from './views/Login';
+import DefaultLayout from './components/DefaultLayout';
+import GuestLayout from './components/GuestLayout';
+import Dashboard from './views/Dashboard';
 
 const router = createBrowserRouter([
     {
-        path: "/login",
-        element: <Login />,
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: "/users",
+                element: <Users />,
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+            {
+                path: '/',
+                element: <Navigate to="/users" />
+            },
+        ]
     },
     {
-        path: "/signup",
-        element: <Signup />,
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <Signup />,
+            }
+        ]
     },
-    {
-        path: "/users",
-        element: <Users />,
-    },
+    
     {
         path: '*',
         element: <NotFound />
